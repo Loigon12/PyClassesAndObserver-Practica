6b58f0d75b697264ea3842a551fd2af49f4d3395
Sebastian Londoño <sebastianpersonal244@gmail.com>
2026-05-17 18:32:12 -0500

    Si

--- src/observer_practice/observer.py
+++ src/observer_practice/observer.py
@@ -2,5 +2,13 @@ from typing import Protocol
 
 
 class Observador(Protocol):
-    def actualizar(self, mensaje):
-        pass
+    def notificar(self, mensaje):
+        for observador in self.observadores:
+            observador.actualizar(mensaje)
+
+    def publicar(self, mensaje):
+        self.ultimo_mensaje = mensaje
+        self.notificar(mensaje)
+        
+    
+    
